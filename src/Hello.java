0fe730f22408acf339cdf5fabffa560ca20e8368
jianghq21 <2071065651@qq.com>
2020-12-21 19:36:26 +0800

    test

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author jhq
+ * @create 2020-12-21 14:47
+ */
+//快捷键  psvm    main方法   ,   sout
+public class Hello {
+    public static void main(String[] args) {
+        int i=2,j=6;
+        int c=i+j;
+        System.out.println("hello");
+        System.out.println(c);
+
+    }
+}
